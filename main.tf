d732abae3fe0acc822a0a0807d9c94069ea96623
Temi <170970335+tolaoluniyi@users.noreply.github.com>
2024-05-30 17:27:54 -0400

    main.tf and script.sh

---
+++ main.tf
@@ -0,0 +1,1 @@
+#Setup EC2 instance with Terraform and implement the user data in the shell script for it to echo a message in a browser.
--- script.sh
+++ script.sh
@@ -4,3 +4,12 @@ sudo yum install httpd -y
 sudo systemctl restart httpd
 sudo systemctl enable httpd
 sudo echo "hello class"> /var/www/html/index.html
+
+
+
+## #/bin/bash
+## sudo apt update -y
+## sudo apt install apache2
+## sudo systemctl restart apache2
+##  sudo systemctl enable apache
+## sudo echo "this is a sample webpage of our installation" > /var/www/html/index.html
